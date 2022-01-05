337cba40e7129f90c89c4a7f094c01ebbc09c892
Pranav Nutalapati <pranavnutalapati@gmail.com>
2022-01-04 22:00:21 -0500

    Now also tracks added things

--- index.mjs
+++ index.mjs
@@ -4,41 +4,41 @@ import { cache } from "./lib/cache.mjs";
 import { CDRAGON, SKIN_SCRAPE_INTERVAL, SUBSTITUTIONS } from "./constants.mjs";
 import { fetchSkinChanges } from "./lib/skin-changes.mjs";
 
-const dataURL = (p) =>
-  `${CDRAGON}/pbe/plugins/rcp-be-lol-game-data/global/default${p}`;
+const dataURL = (p, patch = "pbe") =>
+  `${CDRAGON}/${patch}/plugins/rcp-be-lol-game-data/global/default${p}`;
 
 const substitute = (thing) => SUBSTITUTIONS[thing] ?? thing;
 
-async function getLatestChampions() {
-  const { data } = await axios.get(dataURL("/v1/champion-summary.json"));
-  console.log("[CDragon] Loaded champions.");
+async function getLatestChampions(patch = "pbe") {
+  const { data } = await axios.get(dataURL("/v1/champion-summary.json", patch));
+  console.log(`[CDragon] [${patch}] Loaded champions.`);
   return data
     .filter((d) => d.id !== -1)
     .sort((a, b) => (a.name > b.name ? 1 : -1))
     .map((a) => ({ ...a, key: substitute(a.alias.toLowerCase()) }));
 }
 
-async function getLatestUniverses() {
-  const { data } = await axios.get(dataURL("/v1/universes.json"));
-  console.log("[CDragon] Loaded universes.");
+async function getLatestUniverses(patch = "pbe") {
+  const { data } = await axios.get(dataURL("/v1/universes.json", patch));
+  console.log(`[CDragon] [${patch}] Loaded universes.`);
 
   return data
     .filter((d) => d.id !== 0)
     .sort((a, b) => (a.name > b.name ? 1 : -1));
 }
 
-async function getLatestSkinlines() {
-  const { data } = await axios.get(dataURL("/v1/skinlines.json"));
-  console.log("[CDragon] Loaded skinlines.");
+async function getLatestSkinlines(patch = "pbe") {
+  const { data } = await axios.get(dataURL("/v1/skinlines.json", patch));
+  console.log(`[CDragon] [${patch}] Loaded skinlines.`);
 
   return data
     .filter((d) => d.id !== 0)
     .sort((a, b) => (a.name > b.name ? 1 : -1));
 }
 
-async function getLatestSkins() {
-  const { data } = await axios.get(dataURL("/v1/skins.json"));
-  console.log("[CDragon] Loaded skins.");
+async function getLatestSkins(patch = "pbe") {
+  const { data } = await axios.get(dataURL("/v1/skins.json", patch));
+  console.log(`[CDragon] [${patch}] Loaded skins.`);
 
   Object.keys(data).map((id) => {
     const skin = data[id];
@@ -59,13 +59,28 @@ async function getLatestSkins() {
   return data;
 }
 
-async function getLatestPatchData() {
+async function getLatestPatchData(patch = "pbe") {
   return await Promise.all([
-    getLatestChampions(),
-    getLatestSkinlines(),
-    getLatestSkins(),
-    getLatestUniverses(),
+    getLatestChampions(patch),
+    getLatestSkinlines(patch),
+    getLatestSkins(patch),
+    getLatestUniverses(patch),
   ]);
+}
+
+async function getAdded(champions, skinlines, skins, universes) {
+  const [oldC, oldSl, oldS, oldU] = await getLatestPatchData("latest");
+  const oldSkinIds = new Set(Object.keys(oldS)),
+    oldChampionIds = new Set(oldC.map((c) => c.id)),
+    oldSkinlineIds = new Set(oldSl.map((l) => l.id)),
+    oldUniverseIds = new Set(oldU.map((u) => u.id));
+
+  return {
+    skins: Object.keys(skins).filter((i) => !oldSkinIds.has(i)),
+    champions: champions.map((c) => c.id).filter((i) => !oldChampionIds.has(i)),
+    skinlines: skinlines.map((l) => l.id).filter((i) => !oldSkinlineIds.has(i)),
+    universes: universes.map((u) => u.id).filter((i) => !oldUniverseIds.has(i)),
+  };
 }
 
 async function scrape() {
@@ -88,11 +103,14 @@ async function scrape() {
   } else {
     // Patch changed!
     [champions, skinlines, skins, universes] = await getLatestPatchData();
+    const added = await getAdded(champions, skinlines, skins, universes);
+
     await Promise.all([
       cache.set("champions", champions),
       cache.set("skinlines", skinlines),
       cache.set("skins", skins),
       cache.set("universes", universes),
+      cache.set("added", added),
     ]);
     console.log("[CDragon] Cache updated.");
     shouldRebuild = true;
